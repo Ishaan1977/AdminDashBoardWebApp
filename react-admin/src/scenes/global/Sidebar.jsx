abed6defc458c77fd3a2e530d93e723603a15fee
Ishaan1977 <141465589+Ishaan1977@users.noreply.github.com>
2026-07-21 00:46:33 +0530

    Update Sidebar.jsx

--- react-admin/src/scenes/global/Sidebar.jsx
+++ react-admin/src/scenes/global/Sidebar.jsx
@@ -106,10 +106,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Ishaan Raj
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Database Admin
                 </Typography>
               </Box>
             </Box>
